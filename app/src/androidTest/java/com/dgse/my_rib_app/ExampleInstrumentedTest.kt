4e4cf3469954d2b2d4c107ec6631f9f13a3d16c1
dron247 <bigdigdgs@gmail.com>
2018-04-09 22:44:38 +0800

    init 2

--- app/src/androidTest/java/com/dgse/my_rib_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/dgse/my_rib_app/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.dgse.mybeelinerib", appContext.packageName)
+        assertEquals("com.dgse.my_rib_app", appContext.packageName)
     }
 }
