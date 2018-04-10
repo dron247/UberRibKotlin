a4cbd087e3b65421b2bc2f5d018cc16b5e84fbeb
dron247 <bigdigdgs@gmail.com>
2018-04-10 12:41:24 +0800

    added comparable interface to rib state

--- app/src/main/java/com/dgse/my_rib_app/utility/view_state/RibState.kt
+++ app/src/main/java/com/dgse/my_rib_app/utility/view_state/RibState.kt
@@ -1,17 +1,31 @@
 package com.dgse.my_rib_app.utility.view_state
 
-class RibState(val id: String, val isDefault: Boolean = false, private val block: () -> Unit) {
+class RibState(
+        val id: String,
+        val isDefault: Boolean = false,
+        private val block: () -> Unit) : Comparable<RibState> {
+
     companion object {
         private const val KEY_EMPTY = "empty"
         val Empty = RibState(KEY_EMPTY) {}
     }
 
+    /**
+     * Applies block of the state pn caller side
+     */
     fun apply() {
         block()
     }
 
+    /**
+     * Indicates if current state us useful or it is just empty
+     */
     val isEmpty: Boolean
         get() {
             return KEY_EMPTY == id
         }
+
+    //region Comparable
+    override fun compareTo(other: RibState) = id.compareTo(other.id)
+    //endregion
 }
